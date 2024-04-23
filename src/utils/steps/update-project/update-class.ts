e2382ed12a7b5de6b7e3cd5b8b270608a08de242
ijlee2 <ijlee2@users.noreply.github.com>
2024-04-23 16:03:23 +0200

    chore: Scaffolded the update-class substep

--- src/utils/steps/update-project/update-class.ts
+++ src/utils/steps/update-project/update-class.ts
@@ -1,6 +1,58 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { AST } from '@codemod-utils/ast-javascript';
+
+function updateImportStatement(
+  file: string,
+  data: {
+    isTypeScript: boolean;
+  },
+): {
+  localName: string | undefined;
+  newFile: string;
+} {
+  let localName: string | undefined;
+
+  const traverse = AST.traverse(data.isTypeScript);
+
+  const ast = traverse(file, {
+    // ...
+  });
+
+  return {
+    localName,
+    newFile: AST.print(ast),
+  };
+}
+
+function updateServiceDecorators(
+  file: string,
+  data: {
+    isTypeScript: boolean;
+    localName: string;
+  },
+): string {
+  const traverse = AST.traverse(data.isTypeScript);
+
+  const ast = traverse(file, {
+    // ...
+  });
+
+  return AST.print(ast);
+}
+
 export function updateClass(file: string, isTypeScript: boolean): string {
-  const newFile = file;
+  // eslint-disable-next-line prefer-const
+  let { localName, newFile } = updateImportStatement(file, {
+    isTypeScript,
+  });
+
+  if (!localName) {
+    return file;
+  }
+
+  newFile = updateServiceDecorators(newFile, {
+    isTypeScript,
+    localName,
+  });
 
   return newFile;
 }
