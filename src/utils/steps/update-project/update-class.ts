d18f2dcc2788171cf79167445a84781076f0146a
ijlee2 <ijlee2@users.noreply.github.com>
2024-04-23 16:03:27 +0200

    feature: Found the local name for service

--- src/utils/steps/update-project/update-class.ts
+++ src/utils/steps/update-project/update-class.ts
@@ -1,5 +1,11 @@
 import { AST } from '@codemod-utils/ast-javascript';
 
+function isValueImport(
+  importKind: 'type' | 'typeof' | 'value' | undefined,
+): boolean {
+  return importKind === undefined || importKind === 'value';
+}
+
 function updateImportStatement(
   file: string,
   data: {
@@ -14,7 +20,45 @@ function updateImportStatement(
   const traverse = AST.traverse(data.isTypeScript);
 
   const ast = traverse(file, {
-    // ...
+    visitImportDeclaration(path) {
+      const { importKind, source, specifiers } = path.node;
+
+      if (!isValueImport(importKind)) {
+        return false;
+      }
+
+      if (source.type !== 'Literal' && source.type !== 'StringLiteral') {
+        return false;
+      }
+
+      if (source.value !== '@ember/service' || !Array.isArray(specifiers)) {
+        return false;
+      }
+
+      path.node.specifiers = specifiers.map((specifier) => {
+        // @ts-expect-error: 'specifier.importKind' exists
+        if (!isValueImport(specifier.importKind)) {
+          return specifier;
+        }
+
+        if (specifier.type !== 'ImportSpecifier') {
+          return specifier;
+        }
+
+        if (
+          specifier.imported.name !== 'inject' &&
+          specifier.imported.name !== 'service'
+        ) {
+          return specifier;
+        }
+
+        localName = specifier.local!.name as string;
+
+        return specifier;
+      });
+
+      return false;
+    },
   });
 
   return {
@@ -48,6 +92,8 @@ export function updateClass(file: string, isTypeScript: boolean): string {
   if (!localName) {
     return file;
   }
+
+  console.log(localName);
 
   newFile = updateServiceDecorators(newFile, {
     isTypeScript,
