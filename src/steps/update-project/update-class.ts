b8da0bc7ae58022e431eee176d7c3e54ee7c3953
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-04-30 16:22:19 +0200

    chore: Localized ignore directives

--- src/steps/update-project/update-class.ts
+++ src/steps/update-project/update-class.ts
@@ -37,6 +37,7 @@ function updateImportStatement(
 
       path.node.specifiers = specifiers.map((specifier) => {
         // @ts-expect-error: 'specifier.importKind' exists
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         if (!isValueImport(specifier.importKind)) {
           return specifier;
         }
@@ -80,9 +81,12 @@ function updateServiceDecorators(
     visitCallExpression(node) {
       this.traverse(node);
 
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       switch (node.value.callee.type) {
         case 'Identifier': {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           if (node.value.callee.name === data.localName) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
             node.value.callee.name = 'service';
           }
 
@@ -95,18 +99,24 @@ function updateServiceDecorators(
 
     visitClassProperty(node) {
       if (
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         !Array.isArray(node.value.decorators) ||
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         node.value.decorators.length !== 1
       ) {
         return false;
       }
 
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
       const decorator = node.value.decorators[0];
       let isMatch = false;
 
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       switch (decorator.expression.type) {
         case 'CallExpression': {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           if (decorator.expression.callee.name === data.localName) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
             decorator.expression.callee.name = 'service';
             isMatch = true;
           }
@@ -115,7 +125,9 @@ function updateServiceDecorators(
         }
 
         case 'Identifier': {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
           if (decorator.expression.name === data.localName) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
             decorator.expression.name = 'service';
             isMatch = true;
           }
@@ -129,10 +141,15 @@ function updateServiceDecorators(
       }
 
       // Stylistic choices
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       if (!decorator.trailingComments) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         node.value.accessibility = null;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         node.value.declare = true;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         node.value.definite = null;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         node.value.readonly = null;
       }
 
